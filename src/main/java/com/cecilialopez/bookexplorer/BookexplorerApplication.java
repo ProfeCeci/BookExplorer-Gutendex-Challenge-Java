ece03cd8820c5ec885532c6c0b8e55c0681aef99
Cecilia Lopez <andreacecilopez88@gmail.com>
2025-06-20 18:20:18 -0300

    Initial commit of BookExplorer project

---
+++ src/main/java/com/cecilialopez/bookexplorer/BookexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.cecilialopez.bookexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookexplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cecilialopez/bookexplorer/BookexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cecilialopez.bookexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
